2c735df06bb06ba546f6668677f2112489b65a0f
Mazen Fady <mazenfady85@gmail.com>
2025-07-25 07:11:54 +0300

    Add project files.

---
+++ OOP #05/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ Define 3D Point Class and the basic Constructors (use chaining in constructors).
+
+Override the ToString Function to produce this output:
+Point3D P = new Point3D (10,10,10);
+Console. WriteLine (P. ToString( ));
+Output: “Point Coordinates: (10, 10, 10)”.
+ Read from the User the Coordinates for 2 points P1, P2 (Check the input using try Pares, Parse, Convert).
+
+Try to use ==
+If (P1 == P2)   Does it work properly? 
+Define an array of points and sort this array based on X & Y coordinates.
+Implement ICloneable interface to be able to clone the object.
+To implement more than one interface.
+class Point3D:IComparable ,ICloneable 
+
+
+Second Project:
+Define Class Maths that has four methods: Add, Subtract, Multiply, and Divide, each of them takes two parameters. Call each method in Main ().
+ Modify the program so that you do not have to create an instance of class to call the four methods.
+
+ */
